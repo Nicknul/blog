dedee554e3ae2b5338089e372adc6345fb34b55c
정호연 <jea7330@gmail.com>
2024-05-28 19:14:39 +0900

    test

---
+++ public/test.js
@@ -0,0 +1,8 @@
+// let url = 'http://localhost:3001/submit';
+
+// async function jsonData() {
+//   let a = await fetch(url + '/public/data.json', { method: 'GET' });
+//   let b = await a.json();
+//   return b;
+// }
+// jsonData();
