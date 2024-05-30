e4147f4bd9ae92cff2ce7caf414bf0188c94be55
정호연 <jea7330@gmail.com>
2024-05-30 10:30:12 +0900

    complete method GET

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     console.log('유효성 검사: ', req.url);
+    if (req.url === '/') {
+      fs.readFile('./public/blog.html', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 자체 자체');
+          return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(data);
+      });
+    }
+    if (req.url === '/test.js') {
+      fs.readFile('./public/test.js', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 자체 자체');
+          return;
+        }
+        res.writeHead(200, { 'Content-Type': 'application/javascript; charset=utf-8' });
+        res.end(data);
+      });
+    }
+  } else if (req.method === 'POST') {
+    if (req.url === '/submit') {
+    }
   }
 });
 
