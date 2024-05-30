8acbd88364702fa3c64810309e9b708b5b98cc72
정호연 <jea7330@gmail.com>
2024-05-30 23:33:46 +0900

    edit

--- string.js
+++ string.js
@@ -3,17 +3,7 @@ let string = `<!DOCTYPE html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title></title>
-  </head>
-  <body>
-    <div id="root">
-      <h1></h1>
-      <div></div>
-      <a href="/blog.html">홈</a>
-      <h1>{Menu}</h1>
-    </div>
-  </body>
-</html>
+    <title>
 `;
 
 module.exports = string;
