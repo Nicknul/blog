771c1b3cf2ac6b001c5113683829c0943e422a7c
정호연 <jea7330@gmail.com>
2024-05-30 18:08:22 +0900

    edit

---
+++ string.js
@@ -0,0 +1,19 @@
+let string = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+  </head>
+  <body>
+    <div id="root">
+      <h1></h1>
+      <div></div>
+      <a href="/blog.html">홈</a>
+      <h1>{Menu}</h1>
+    </div>
+  </body>
+</html>
+`;
+
+module.exports = string;
