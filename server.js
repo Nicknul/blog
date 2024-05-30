0c9111a8ad3a2c0c82f41705678398af64d0a7f4
정호연 <jea7330@gmail.com>
2024-05-30 10:18:22 +0900

    code edited

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    console.log('유효성 검사: ', req.url);
+  }
+});
+
+const port = 3000;
+server.listen(port, (err) => {
+  if (err) {
+    console.error('File Error');
+  } else {
+    console.log('서버 돌아가는 중');
+    console.log(`http://localhost:${port}`);
+  }
+});
