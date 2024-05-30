75ac17d54a2bdc9529dddc42f721017a65087419
정호연 <jea7330@gmail.com>
2024-05-30 14:51:20 +0900

    test

--- test-2.js
+++ test-2.js
@@ -1,4 +1,15 @@
-const fs = require('fs');
-const data = fs.readFileSync('./blog.html', 'utf-8');
+// const fs = require('fs');
+// const data = fs.readFileSync('./public/blog.html', 'utf-8');
 
-console.log(data);
+// console.log(data);
+
+// let div = `</form>
+//     <div id="root">
+//       <a href="/hi.html">바로가기</a>
+//     </div>`;
+
+// let a = data.replace('</form>', div);
+
+// console.log(a);
+
+// module.exports = a;
