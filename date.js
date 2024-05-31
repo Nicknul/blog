a3813810f77758b1576576381d98451bab0c5028
정호연 <jea7330@gmail.com>
2024-05-31 14:01:51 +0900

    add code

--- date.js
+++ date.js
@@ -1,9 +1,14 @@
 let newDate = new Date();
+// console.log(newDate);
 
 let year = newDate.getFullYear();
 let month = newDate.getMonth() + 1;
 let date = newDate.getDate();
+let hours = newDate.getHours();
+let minutes = newDate.getMinutes();
+let seconds = newDate.getSeconds();
+// console.log(a);
 
-let today = `${year}-${month}-${date}`;
+let today = `${year}-${month}-${date}-${hours}-${minutes}-${seconds}`;
 
 module.exports = today;
