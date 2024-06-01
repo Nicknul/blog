9e5acfcf2395919e0ba98198125fb1c9585f0b08
정호연 <jea7730@gmail.com>
2024-06-01 23:50:57 +0900

    error

--- server.js
+++ server.js
@@ -46,20 +46,25 @@ const dataStr = `<!DOCTYPE html>
 let list = '';
 let link = '';
 
-// fs.readdir('./data', (err, fileList) => {
-//   for (let names in fileList) {
-//     console.log(fileList[names]);
-//   }
-// });
+fs.readdir('./data', (err, fileList) => {
+  link = fileList;
+  for (let names in fileList) {
+    list += `<li><a href="/data/${fileList[names]}">${fileList[names]}</a></li>`;
+  }
+});
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     console.log('유효성 검사:', req.url);
     if (req.url === '/') {
-      let add = blogStr.replace('<div></div>', `${list}`);
-      fs.writeFile('./blog.html', add, 'utf-8', (err) => {
+      // let add = blogStr.replace('<div></div>', `${list}`);
+      // fs.writeFile('./blog.html', add, 'utf-8', (err) => {
+      //   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      //   res.end(add);
+      // });
+      fs.readFile('./blog.html', (err, data) => {
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(add);
+        res.end(data);
       });
     }
     for (let element in link) {
@@ -89,88 +94,17 @@ const server = http.createServer((req, res) => {
         let b = a.replace('<h1>name</h1>', `<h1>${title}</h1>`);
         let c = b.replace('<div>content</div>', `<div>${content}</div>`);
 
-        let add = blogStr.replace('<div></div>', `${list}`);
-
         // let e = [];
-        // fs.readdir('./data', 'utf-8', (err, fileList) => {
-        //   e += fileList;
-        //   console.log(e);
-        // });
 
-        fs.writeFile(`./data/${today}.html`, c, 'utf-8', (err) => {
-          if (err) {
-            console.log(err);
-          }
-          fs.readdir('./data', (err, fileList) => {
-            for (let element in fileList) {
-              console.log(fileList[element]);
-            }
-          });
-          fs.writeFile('./blog.html', add, 'utf-8', (err) => {
-            if (err) {
-              console.log(err);
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.end(add);
-          });
-          // fs.readFile('./blog.html', (err, data) => {
-          //   if (err) {
-          //     console.log(err);
-          //   }
-          //   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-          //   res.end(data);
-          // });
-          // res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-          // res.end();
+        // fs.readdir('./data', (err, fileList) => {
+        //   for (let element in fileList) {
+        //     console.log(fileList[element]);
+        //   }
+        // });
+        fs.writeFile(`./data/${today}.html`, c, 'utf-8', () => {
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+          res.end(c);
         });
-        // fs.writeFile('./blog.html', add, 'utf-8', () => {
-        //   fs.writeFileSync(`./data/${today}.html`, c, 'utf-8');
-        //   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        //   res.end(add);
-        // });
-
-        // //* blog.html menu
-        // let blogFileStr = `<!DOCTYPE html>
-        // <html lang="en">
-        //   <head>
-        //     <meta charset="UTF-8" />
-        //     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        //     <title>blog</title>
-        //   </head>
-        //   <body>
-        //     <form action="/submit" method="post">
-        //       <label for="title">제목</label><br />
-        //       <input type="text" name="title" id="title" /><br /><br />
-        //       <label for="content">내용</label><br />
-        //       <textarea type="text" name="content" id="content"></textarea> <br />
-        //       <br />
-        //       <input type="submit" value="포스팅 하기" />
-        //     </form>
-        //     <div id="root">
-        //       <h1>Menu</h1>
-        //       <a href="./data/${today}.html">${title}</a>
-        //     </div>
-        //   </body>
-        // </html>
-        // `;
-
-        // fs.writeFile('./blog.html', blogFileStr, 'utf-8', () => {
-        //   const read = fs.readFileSync('./blog.html', 'utf-8');
-
-        //   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        //   res.end(read);
-        // });
-
-        // let blog = require('./blog.js');
-        // let menu = `<a href="/data/${today}.html">${title}</a>`;
-        // let addMenu = blog.replace('array', menu);
-
-        // fs.writeFile('./blog.html', addMenu, 'utf-8', () => {
-        //   let newBlog = fs.readFileSync('./blog.html', 'utf-8');
-
-        //   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        //   res.end(newBlog);
-        // });
       });
     }
   }
